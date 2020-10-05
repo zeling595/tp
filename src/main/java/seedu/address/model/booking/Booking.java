f3183e7a51614033d3b0fe0a11003993fca4e785
SherryWu178 <sherry.wu178@gmail.com>
2020-10-05 20:46:52 +0800

    Add Booking class

---
+++ src/main/java/seedu/address/model/booking/Booking.java
@@ -0,0 +1,128 @@
+package seedu.address.model.booking;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+
+public class Booking {
+    private static Integer nextAvailableId;
+
+    // Identity fields
+    private final Integer id;
+    private final Integer roomId;
+    private final Integer personId;
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+    private final Boolean isActive;
+
+
+    /**
+     * Every field must be present and not null. Used for creating a new Booking with a unique id.
+     */
+    public Booking(Integer roomId, Integer personId, LocalDate startDate, LocalDate endDate, Boolean isActive) {
+        requireAllNonNull(roomId, personId, startDate, endDate);
+        this.roomId = roomId;
+        this.personId = personId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.isActive = isActive;
+        this.id = nextAvailableId;
+        nextAvailableId += 1;
+    }
+
+    /**
+     * Every field must be present and not null. Used for creating a Booking with an existing id.
+     */
+    public Booking(Integer roomId, Integer personId, LocalDate startDate, LocalDate endDate, Boolean isActive,
+                   Integer id) {
+        requireAllNonNull(roomId, personId, startDate, endDate, id);
+        this.roomId = roomId;
+        this.personId = personId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.isActive = isActive;
+        this.id = id;
+    }
+
+    /**
+     * Sets next available id to be used
+     */
+    public static void setNextAvailableId(Integer id) {
+        Booking.nextAvailableId = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getRoomId() {
+        return roomId;
+    }
+
+    public Integer getPersonId() {
+        return personId;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+
+    /**
+     * Returns true if both bookings have the same identity and data fields.
+     * This defines a stronger notion of equality between two persons.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Booking)) {
+            return false;
+        }
+
+        Booking otherBooking = (Booking) other;
+        return otherBooking.getId().equals(getId())
+                && otherBooking.getRoomId().equals(getRoomId())
+                && otherBooking.getPersonId().equals(getPersonId())
+                && otherBooking.getStartDate().equals(getStartDate())
+                && otherBooking.getEndDate().equals(getEndDate())
+                && otherBooking.isActive();
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(roomId, personId, startDate, endDate, isActive);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("Id: ")
+                .append(getId())
+                .append(" Room Id: ")
+                .append(getRoomId())
+                .append(" Person Id: ")
+                .append(getPersonId())
+                .append(" Start Date: ")
+                .append(getStartDate())
+                .append(" End Date: ")
+                .append(getEndDate())
+                .append(" Active: ")
+                .append(isActive());
+        return builder.toString();
+    }
+
+}
