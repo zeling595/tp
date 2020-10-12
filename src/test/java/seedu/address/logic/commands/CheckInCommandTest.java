e929e7c3948444cf2fc2c7eae7808d9af6d766c1
gene <iamgenechua@gmail.com>
2020-10-13 00:05:57 +0800

    Fix CheckInCommandTests

--- src/test/java/seedu/address/logic/commands/CheckInCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CheckInCommandTest.java
@@ -1,26 +1,32 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.CheckInCommand.MESSAGE_ARGUMENTS;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_END_DATE_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_END_DATE_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_END_DATE_GENE;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PERSONAL_ID_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PERSONAL_ID_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PERSONAL_ID_GENE;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ROOM_ID_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ROOM_ID_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ROOM_ID_GENE;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_START_DATE_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_START_DATE_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_START_DATE_GENE;
 import static seedu.address.testutil.TypicalBookings.getTypicalBookingBook;
+import static seedu.address.testutil.TypicalPersons.GENE;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 import static seedu.address.testutil.TypicalRooms.getTypicalRoomBook;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.booking.Booking;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for CheckInCommand.
@@ -29,12 +35,22 @@ public class CheckInCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs(), getTypicalRoomBook(),
             getTypicalBookingBook());
 
-    @Test
-    public void execute() {
-        assertCommandFailure(new CheckInCommand(VALID_PERSONAL_ID_AMY, VALID_ROOM_ID_AMY,
-                VALID_START_DATE_AMY, VALID_END_DATE_AMY), model,
-                String.format(MESSAGE_ARGUMENTS, VALID_PERSONAL_ID_AMY, VALID_ROOM_ID_AMY,
-                        VALID_START_DATE_AMY, VALID_END_DATE_AMY));
+    @Test void execute_success() throws CommandException {
+        model.addPerson(GENE);
+
+        // stub
+        // when i create a new booking on line 42, my booking ID++
+        // so when i assertEquals, i fail because my checkInCommand will create a booking with the incremented ID
+        final int testBookingId = 7;
+
+        Booking booking = new Booking(VALID_ROOM_ID_GENE, VALID_PERSONAL_ID_GENE,
+                VALID_START_DATE_GENE, VALID_END_DATE_GENE, true, testBookingId);
+
+        CheckInCommand command = new CheckInCommand(VALID_PERSONAL_ID_GENE, VALID_ROOM_ID_GENE,
+                VALID_START_DATE_GENE, VALID_END_DATE_GENE);
+
+        assertEquals(String.format(CheckInCommand.MESSAGE_SUCCESS, booking),
+                command.execute(model).getFeedbackToUser());
     }
 
     @Test
