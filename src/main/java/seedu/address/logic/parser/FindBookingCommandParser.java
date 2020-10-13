124a32f2ad19aa901077b025c5a221e6eb62482b
SherryWu178 <sherry.wu178@gmail.com>
2020-10-13 20:32:17 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/parser/FindBookingCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindBookingCommandParser.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
-import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.FindBookingCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.booking.Booking;
