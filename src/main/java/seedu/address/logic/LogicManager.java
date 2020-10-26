8f238877af6790aaa184b6fdd6d334c3b9c4ef39
shermzlim <shermz.lim.jh@gmail.com>
2020-10-27 02:22:28 +0800

    Add home page styling

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -14,7 +14,6 @@ import seedu.address.logic.parser.AddressBookParser;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
 import seedu.address.model.ReadOnlyAddressBook;
-import seedu.address.model.ReadOnlyBookingBook;
 import seedu.address.model.booking.Booking;
 import seedu.address.model.person.Person;
 import seedu.address.storage.Storage;
@@ -74,7 +73,9 @@ public class LogicManager implements Logic {
     }
 
     @Override
-    public ObservableList<Booking> getBookingList() { return model.getBookingBook().getBookingList(); }
+    public ObservableList<Booking> getBookingList() {
+        return model.getBookingBook().getBookingList();
+    }
 
     @Override
     public Path getAddressBookFilePath() {
--- src/main/java/seedu/address/ui/HomePagePanel.java
+++ src/main/java/seedu/address/ui/HomePagePanel.java
@@ -13,15 +13,39 @@ public class HomePagePanel extends UiPart<Region> {
     private static final String FXML = "HomePagePanel.fxml";
 
     @FXML
-    private ListView recentBookingsView;
+    private Label roomsAvailableTitle;
+
+    @FXML
+    private Label singleRoomsText;
+
+    @FXML
+    private Label doubleRoomsText;
+
+    @FXML
+    private Label suiteRoomsText;
+
+    @FXML
+    private Label recentBookingsTitle;
+
+    @FXML
+    private ListView<Booking> recentBookingsView;
+
 
     /**
      * Creates a HomePagePanel
      */
     public HomePagePanel(ObservableList<Booking> bookingList) {
         super(FXML);
+        roomsAvailableTitle.setText("Number of Rooms Available:");
+        singleRoomsText.setText(
+                "10 Single Rooms");
+        doubleRoomsText.setText(
+                "10 Double Rooms");
+        suiteRoomsText.setText(
+                "10 Suite Rooms");
+        recentBookingsTitle.setText("Recently Checked In:");
         SortedList<Booking> sortedBookingList = new SortedList<>(bookingList, (o1, o2)
-                -> o2.getStartDate().compareTo(o1.getStartDate()));
+            -> o2.getStartDate().compareTo(o1.getStartDate()));
         recentBookingsView.setItems(sortedBookingList);
         recentBookingsView.setCellFactory(listView -> new RecentBookingCell());
     }
@@ -29,7 +53,7 @@ public class HomePagePanel extends UiPart<Region> {
     class RecentBookingCell extends ListCell<Booking> {
         @Override
         protected void updateItem(Booking booking, boolean empty) {
-            if (getIndex() > 2) {
+            if (getIndex() > 4) {
                 setGraphic(null);
                 setText(null);
                 return;
@@ -40,7 +64,11 @@ public class HomePagePanel extends UiPart<Region> {
                 setGraphic(null);
                 setText(null);
             } else {
-                setGraphic(new Label(booking.toString()));
+                setGraphic(new Label(
+                        String.format("%s. Person %s, Room Number %s, %s",
+                                getIndex() + 1, booking.getPersonId(),
+                                booking.getRoomId(), booking.getStartDate().toString())
+                ));
             }
         }
     }
