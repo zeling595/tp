4ac8dd009ebb8f2f9b5eae3280c80a5593e3a789
zeling595 <longzeling595@gmail.com>
2020-10-27 22:12:18 +0800

    catch duplicate booking exception in checkin command

--- src/main/java/seedu/address/logic/commands/CheckInCommand.java
+++ src/main/java/seedu/address/logic/commands/CheckInCommand.java
@@ -1,8 +1,6 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.commons.core.Messages.MESSAGE_CONFLICTING_BOOKING;
-import static seedu.address.commons.core.Messages.MESSAGE_PERSONAL_ID_MISSING;
-import static seedu.address.commons.core.Messages.MESSAGE_ROOM_ID_MISSING;
+import static seedu.address.commons.core.Messages.*;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_END_DATE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PERSONAL_ID;
@@ -15,6 +13,7 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.booking.Booking;
 import seedu.address.model.booking.exception.ConflictingBookingException;
+import seedu.address.model.booking.exception.DuplicateBookingException;
 
 /**
  * Encapsulates the Check In feature.
@@ -87,6 +86,9 @@ public class CheckInCommand extends Command {
         } catch (ConflictingBookingException e) {
             Booking.setNextAvailableId(bookingId);
             throw new CommandException(MESSAGE_CONFLICTING_BOOKING);
+        } catch (DuplicateBookingException de) {
+            Booking.setNextAvailableId(bookingId);
+            throw new CommandException(MESSAGE_DUPLICATE_BOOKING);
         }
     }
 
