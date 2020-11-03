f7b3aaff7454a45105cd725c5eeabe8512f021b1
gene <iamgenechua@gmail.com>
2020-11-03 19:19:04 +0800

    Add success testcases with different variants of wifi, dining, massage

--- src/test/java/seedu/address/logic/parser/RoomServiceCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/RoomServiceCommandParserTest.java
@@ -38,4 +38,67 @@ class RoomServiceCommandParserTest {
         assertParseSuccess(
                 parser, input3, new RoomServiceCommand(new RoomService(1, RoomServiceType.MASSAGE)));
     }
+
+    @Test
+    public void parse_validArgsDifferentCasesSuccess() {
+        String wifiVariant1 = "wiFi";
+        String wifiVariant2 = "wIFi";
+        String wifiVariant3 = "WifI";
+        String diningVariant1 = "diNinG";
+        String diningVariant2 = "DINing";
+        String diningVariant3 = "dinINg";
+        String massageVariant1 = "mASSaGE";
+        String massageVariant2 = "mASsaGe";
+        String massageVariant3 = "MASSage";
+
+        String input = " " + PREFIX_BOOKING_ID + "1 " + PREFIX_ROOM_SERVICE_TYPE + wifiVariant1;
+        assertParseSuccess(
+                parser, input, new RoomServiceCommand(new RoomService(1, RoomServiceType.WIFI)));
+
+        input = " " + PREFIX_BOOKING_ID + "1 " + PREFIX_ROOM_SERVICE_TYPE + wifiVariant2;
+        assertParseSuccess(
+                parser, input, new RoomServiceCommand(new RoomService(1, RoomServiceType.WIFI)));
+
+        input = " " + PREFIX_BOOKING_ID + "1 " + PREFIX_ROOM_SERVICE_TYPE + wifiVariant3;
+        assertParseSuccess(
+                parser, input, new RoomServiceCommand(new RoomService(1, RoomServiceType.WIFI)));
+
+        input = " " + PREFIX_BOOKING_ID + "1 " + PREFIX_ROOM_SERVICE_TYPE + diningVariant1;
+        assertParseSuccess(
+                parser, input, new RoomServiceCommand(new RoomService(1, RoomServiceType.DINING)));
+
+        input = " " + PREFIX_BOOKING_ID + "1 " + PREFIX_ROOM_SERVICE_TYPE + diningVariant2;
+        assertParseSuccess(
+                parser, input, new RoomServiceCommand(new RoomService(1, RoomServiceType.DINING)));
+
+        input = " " + PREFIX_BOOKING_ID + "1 " + PREFIX_ROOM_SERVICE_TYPE + diningVariant3;
+        assertParseSuccess(
+                parser, input, new RoomServiceCommand(new RoomService(1, RoomServiceType.DINING)));
+
+        input = " " + PREFIX_BOOKING_ID + "1 " + PREFIX_ROOM_SERVICE_TYPE + massageVariant1;
+        assertParseSuccess(
+                parser, input, new RoomServiceCommand(new RoomService(1, RoomServiceType.MASSAGE)));
+
+        input = " " + PREFIX_BOOKING_ID + "1 " + PREFIX_ROOM_SERVICE_TYPE + massageVariant2;
+        assertParseSuccess(
+                parser, input, new RoomServiceCommand(new RoomService(1, RoomServiceType.MASSAGE)));
+
+        input = " " + PREFIX_BOOKING_ID + "1 " + PREFIX_ROOM_SERVICE_TYPE + massageVariant3;
+        assertParseSuccess(
+                parser, input, new RoomServiceCommand(new RoomService(1, RoomServiceType.MASSAGE)));
+    }
+
+    @Test
+    public void parse_invalidRoomServiceTypeOne_throwsParseException() {
+        String input = " " + PREFIX_BOOKING_ID + "1 " + PREFIX_ROOM_SERVICE_TYPE + "Escorts";
+        assertParseFailure(parser, input, String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                RoomServiceCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidRoomServiceTwo_throwsParseException() {
+        String input = " " + PREFIX_BOOKING_ID + "2 " + PREFIX_ROOM_SERVICE_TYPE + "wife";
+        assertParseFailure(parser, input, String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                RoomServiceCommand.MESSAGE_USAGE));
+    }
 }
